079ea37ff9605abd90b71e4d3f6beb4ad194e4be
Maksim <122976125+Maxim12e@users.noreply.github.com>
2023-09-08 18:25:57 +0300

    Код для вычисления високосного года

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-Print("ONETwo")
+year = int(input("Введіть рік: "))
+
+def is_leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return print(f"{year} - високосний рік")
+    else:
+        return print(f"{year} - не високосний рік")
+
+
+
